aa4531ff9e10d707695f14a536163f68af8f0fbe
Chanakyha <vetrichanakyha2003@gmail.com>
2022-12-13 18:09:51 +0530

    Changed the methods to targeted audience

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from instabot import Bot
 import json
 
 credentials = json.load(open('credentials.json'))
-username,password = credentials["username"],credentials["password"]
+username,password,targets = credentials["username"],credentials["password"],credentials["target"]
 
 
 bot = Bot()
@@ -10,28 +10,32 @@ bot = Bot()
 # Login to Instagram
 bot.login(username=username, password=password)
 
-followersIDs = bot.get_user_followers(username)
+for target in targets:
 
-for each_follower in followersIDs:
-    bot.follow(each_follower) # Follow each follower
+    followersIDs = bot.get_user_followers(target)
 
-    sleep(1200) # Sleep for 15 seconds
+    for count,each_follower in enumerate(followersIDs):
+        if count > 100:
+            break
+        bot.follow(each_follower) # Follow each follower
+
+        sleep(1200) # Sleep for 15 seconds
 
 
-    messasge_text = f'''Hello {bot.get_username_from_user_id(each_follower)}, do you need a person to outsource work to?
+        messasge_text = f'''Hello {bot.get_username_from_user_id(each_follower)}, do you need a person to outsource work to?
 
-Like copywriting, social media management, graphic design, video/reel editing, web development, funnel creation etc?
+    Like copywriting, social media management, graphic design, video/reel editing, web development, funnel creation etc?
 
-Hi (username) , I'm Roy an engineering student looking for opportunities to assist you with my skills. Can we have a chat to see if we fit and compliment each other to grow?
+    Hi (username) , I'm Roy an engineering student looking for opportunities to assist you with my skills. Can we have a chat to see if we fit and compliment each other to grow?
 
-👉 Social media manager and growth
-👉 Content creation
-👉 Website/ App development
-👉 Graphic designers
-👉 Video editors
-👉 Virtual assistants'''
-    bot.send_messages(messasge_text, [each_follower])
+    👉 Social media manager and growth
+    👉 Content creation
+    👉 Website/ App development
+    👉 Graphic designers
+    👉 Video editors
+    👉 Virtual assistants'''
+        bot.send_messages(messasge_text, [each_follower])
 
-    print(f"message sent to {bot.get_username_from_user_id(each_follower)}")
+        print(f"message sent to {bot.get_username_from_user_id(each_follower)}")
 
 bot.logout()
