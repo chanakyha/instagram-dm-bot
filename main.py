3e43573e6a3551cca542498bf329f9bdb355bd69
Chanakyha <vetrichanakyha2003@gmail.com>
2022-12-13 17:10:32 +0530

    Imported the credentials files to main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from instabot import Bot
+import json
+
+credentials = json.load(open('credentials.json'))
+
 
 bot = Bot()
+
+# Login to Instagram
+bot.login(username=credentials["username"], password=credentials["password"])
