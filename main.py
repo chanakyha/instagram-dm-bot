0ae2f19c76cb6ab16e652dfe1a42db819f8035b7
Chanakyha <vetrichanakyha2003@gmail.com>
2022-12-13 17:32:52 +0530

    completed Building Instagram Bo

--- main.py
+++ main.py
@@ -2,9 +2,36 @@ from instabot import Bot
 import json
 
 credentials = json.load(open('credentials.json'))
+username,password = credentials["username"],credentials["password"]
 
 
 bot = Bot()
 
 # Login to Instagram
-bot.login(username=credentials["username"], password=credentials["password"])
+bot.login(username=username, password=password)
+
+followersIDs = bot.get_user_followers(username)
+
+for each_follower in followersIDs:
+    bot.follow(each_follower) # Follow each follower
+
+    sleep(1200) # Sleep for 15 seconds
+
+
+    messasge_text = f'''Hello {bot.get_username_from_user_id(each_follower)}, do you need a person to outsource work to?
+
+Like copywriting, social media management, graphic design, video/reel editing, web development, funnel creation etc?
+
+Hi (username) , I'm Roy an engineering student looking for opportunities to assist you with my skills. Can we have a chat to see if we fit and compliment each other to grow?
+
+👉 Social media manager and growth
+👉 Content creation
+👉 Website/ App development
+👉 Graphic designers
+👉 Video editors
+👉 Virtual assistants'''
+    bot.send_messages(messasge_text, [each_follower])
+
+    print(f"message sent to {bot.get_username_from_user_id(each_follower)}")
+
+bot.logout()
